eb6479f9a18384e9cefec30055a60265cfff63d2
carlos <carlosleonfranco13@gmail.com>
2023-03-27 16:05:14 -0500

    Llenar las opciones de años con UI prototype

---
+++ js/app.js
@@ -0,0 +1,30 @@
+// CONSTRUCTORES
+function seguro(marca, year, tipo){
+    this.marca = marca;
+    this.year = year;
+    this.tipo = tipo;
+}
+function UI() {}
+
+// Llena las opciones de los años
+UI.prototype.llenarOpciones = () => {
+    const max = new Date().getFullYear();
+    const min = max - 20;
+
+    const selectYear = document.querySelector('#year');
+
+    for(let i = max; i > min; i--) {
+        let opcion = document.createElement('option');
+        opcion.value = i;
+        opcion.textContent = i;
+        selectYear.appendChild(opcion);
+    }
+}
+
+// Instanciar UI 
+const ui = new UI();
+
+
+document.addEventListener('DOMContentLoaded', () => {
+    ui.llenarOpciones(); // Llena el select con los años...
+})
